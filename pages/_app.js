210783752792cc2b8e7bee8862176d0ccddbf4cf
Sangwon7242 <gotkdqja@gmail.com>
2023-03-12 10:24:20 +0900

    연락처 페이지 추가, 공통상단 작업

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,27 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Link from "next/link";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="description" content="할일관리서비스 투두입니다." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <nav>
+        <Link href="/" legacyBehavior>
+          <a>메인</a>
+        </Link>
+        <Link href="/about" legacyBehavior>
+          <a style={{ color: "red" }}>어바웃</a>
+        </Link>
+        <Link href="/contact" legacyBehavior>
+          <a style={{ color: "pink" }}>연락처</a>
+        </Link>
+      </nav>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,12 @@
 import Head from "next/head";
-import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>메인 | 투두</title>
-        <meta name="description" content="할일관리서비스 투두입니다." />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>메인 페이지</h1>
-
-      <nav>
-        <Link href="/" legacyBehavior>
-          <a style={{ color: "red" }}>메인</a>
-        </Link>
-        <Link href="/about" legacyBehavior>
-          <a>어바웃</a>
-        </Link>
-      </nav>
     </>
   );
 }
