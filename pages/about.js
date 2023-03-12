5613cf7e08fe510eaba3e1d14ce220355ff79189
Sangwon7242 <gotkdqja@gmail.com>
2023-03-12 10:19:18 +0900

    Link 엘리먼트를 이용한 우아한 페이지 이동, prettier를 이용해서 코드포맷

--- pages/about.js
+++ pages/about.js
@@ -1,23 +1,24 @@
 import Head from "next/head";
 import Link from "next/link";
 
-export default function Home() {
+export default function About() {
   return (
     <>
       <Head>
-        <title>메인 | 투두</title>
+        <title>앱소개 | 투두</title>
         <meta name="description" content="할일관리서비스 투두입니다." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>메인 페이지</h1>
+
+      <h1>앱소개 페이지</h1>
 
       <nav>
         <Link href="/" legacyBehavior>
-          <a style={{ color: "red" }}>메인</a>
+          <a>메인</a>
         </Link>
         <Link href="/about" legacyBehavior>
-          <a>어바웃</a>
+          <a style={{ color: "red" }}>어바웃</a>
         </Link>
       </nav>
     </>
